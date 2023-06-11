b8fea686fb05dd9103fdde99d573648a2d34fe0a
Julie Sanetrníková <sanj10@vse.cz>
2023-06-11 14:48:58 +0200

    oprava props v App.tsx atd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // im
 import './App.css';
 import Match from './components/match/Match';
 import LeagueList from './components/LeagueList';
-
 import Navbar from './components/HomePagePackage/Navbar';
 import HomePage from './components/HomePagePackage/HomePage';
 //import PrihlaseniPage from './components/PrihlaseniPagePackage/PrihlaseniPage';
 //import RegistracePage from './components/RegistracePagePackage/RegistracePage';
 
-interface AppProps {
-    HomePage: ReactNode,
-    PrihlaseniPage: ReactNode,
-    RegistracePage: ReactNode,
-    navbarPrihlaseniPage: ReactNode,
-    navbarRegistracePage: ReactNode
-}
-
-const App = ({ HomePage, navbarPrihlaseniPage, navbarRegistracePage }: AppProps) => {
+const App = () => {
     // @ts-ignore
     return (
         <Router>
-            <Navbar HomePage={HomePage} PrihlaseniPage={navbarPrihlaseniPage} RegistracePage={navbarRegistracePage}/>
             <Routes>
-                <Route path="/" element={HomePage} />
+                <Route path="/" element={<HomePage  HomePage="" PrihlaseniPage="" RegistracePage=""/>} />
                 {/* <Route path="/prihlaseni" element={<PrihlaseniPage />} />
-            <Route path="/registrace" element={<RegistracePage />} />*/}
+                <Route path="/registrace" element={<RegistracePage />} />*/}
             </Routes>
         </Router>
     );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App HomePage="" PrihlaseniPage="" RegistracePage="" navbarPrihlaseniPage="" navbarRegistracePage=""/>
+    <App />
   </React.StrictMode>
 );
 
