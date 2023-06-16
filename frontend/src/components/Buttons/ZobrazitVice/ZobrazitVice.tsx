2c6ae8c1cc50709e6b6f422487ca3f21ea31afd5
Julie Sanetrníková <sanj10@vse.cz>
2023-06-16 16:27:13 +0200

    upravene tlacitko zobrazit vice

--- frontend/src/components/Buttons/ZobrazitVice/ZobrazitVice.tsx
+++ frontend/src/components/Buttons/ZobrazitVice/ZobrazitVice.tsx
@@ -6,13 +6,15 @@ import chevron from '../../../assets/chevron-right.svg';
 function ZobrazitVice(handleClick: any) {
     return (
         <div onClick={handleClick} className={`flex items-center ${styles.text}`}>
-            <button className={`${styles.text} uppercase pr-4`}>Zobrazit více</button>
-            <div>
-                <svg width="10" height="16" fill="black" className={`${styles.icon}`}
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10 8L1.51943 16L0 14.5667L6.99647 8L0.0353357 1.43333L1.55477 -1.66893e-06L10 8Z"/>
-                </svg>
-            </div>
+            <button className={`${styles.text} uppercase pr-4`}>Zobrazit více
+                <div>
+                    <svg width="10" height="16" fill="black" className={`${styles.icon}`}
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 8L1.51943 16L0 14.5667L6.99647 8L0.0353357 1.43333L1.55477 -1.66893e-06L10 8Z"/>
+                    </svg>
+                </div>
+            </button>
+
         </div>
     )
 }
