c1bd324c1d5f155225c2e3c5ba7102168a3c918d
Adam <adam.skarvada@seznam.cz>
2023-06-20 17:18:47 +0200

    Komponenta detailního skóre

--- frontend/src/components/MatchDetailPagePackage/MainSection.tsx
+++ frontend/src/components/MatchDetailPagePackage/MainSection.tsx
@@ -5,92 +5,126 @@ import './Navbar.css';
 import './MatchDetailPage.css';
 import ContentHolder from "../BrowsingContentHolder/ContentHolder";
 import styles from "../Match/Match.module.css";
+import sparta from '../../assets/sparta.png';
+import slavia from '../../assets/slavia.png';
+import MatchDetailedScore from "../MatchDetailedScore/MatchDetailedScore";
 
 interface BasketballMatchData {
+    id: number,
     date: any,
     time: any,
     homeTeamId: number,
     awayTeamId: number,
     homeTeamName: string,
     awayTeamName: string,
-    homeTeamScore: number | null,
-    awayTeamScore: number | null,
     homeTeamLogo: string | null,
     awayTeamLogo: string | null,
+    alreadyPlayed: boolean,
+    isFavourite: boolean,
+    score : {
+        finalAwayScore: number,
+        finalHomeScore: number,
+        firstQuarterAwayScore: number,
+        firstQuarterHomeScore: number,
+        secondQuarterAwayScore: number,
+        secondQuarterHomeScore: number,
+        thirdQuarterAwayScore: number,
+        thirdQuarterHomeScore: number,
+        fourthQuarterAwayScore: number,
+        fourthQuarterHomeScore: number,
+        overtimeAwayScore: number,
+        overtimeHomeScore: number
+    }
 
-
-    id: number,
-    finalAwayScore: number,
-    finalHomeScore: number,
-    firstQuarterAwayScore: number,
-    firstQuarterHomeScore: number,
-    secondQuarterAwayScore: number,
-    secondQuarterHomeScore: number,
-    thirdQuarterAwayScore: number,
-    thirdQuarterHomeScore: number,
-    fourthQuarterAwayScore: number,
-    fourthQuarterHomeScore: number,
-    overtimeAwayScore: number,
-    overtimeHomeScore: number
 }
 
 interface HockeyMatchData {
+    id: number,
     date: any,
     time: any,
     homeTeamId: number,
     awayTeamId: number,
     homeTeamName: string,
     awayTeamName: string,
-    homeTeamScore: number | null,
-    awayTeamScore: number | null,
     homeTeamLogo: string | null,
     awayTeamLogo: string | null,
-
-    id: number,
-    finalAwayScore: number,
-    finalHomeScore: number,
-    firstPeriodAwayScore: number,
-    firstPeriodHomeScore: number,
-     secondPeriodAwayScore : number,
-     secondPeriodHomeScore : number,
-     thirdPeriodAwayScore : number,
-     thirdPeriodHomeScore : number,
-     overtimeAwayScore : number,
-     overtimeHomeScore : number,
-     shootoutAwayScore : number,
-     shootoutHomeScore : number
+    alreadyPlayed: boolean,
+    isFavourite: boolean,
+    score : {
+        finalAwayScore: number,
+        finalHomeScore: number,
+        firstPeriodAwayScore: number,
+        firstPeriodHomeScore: number,
+        secondPeriodAwayScore: number,
+        secondPeriodHomeScore: number,
+        thirdPeriodAwayScore: number,
+        thirdPeriodHomeScore: number,
+        overtimeAwayScore: number,
+        overtimeHomeScore: number,
+        shootoutAwayScore: number,
+        shootoutHomeScore: number
+    }
 }
 
 interface FootballMatchData {
+    id: number,
     date: any,
     time: any,
     homeTeamId: number,
     awayTeamId: number,
     homeTeamName: string,
     awayTeamName: string,
-    homeTeamScore: number | null,
-    awayTeamScore: number | null,
     homeTeamLogo: string | null,
     awayTeamLogo: string | null,
-
-     id : number,
-     finalAwayScore : number,
-     finalHomeScore: number,
-     firstHalfAwayScore : number,
-     firstHalfHomeScore : number,
-     secondHalfAwayScore  : number,
-     secondHalfHomeScore : number,
-     overtimeAwayScore : number,
-     overtimeHomeScore : number,
-     penaltyAwayScore : number,
-     penaltyHomeScore : number
+    alreadyPlayed: boolean,
+    isFavourite: boolean,
+    score : {
+        finalAwayScore: number,
+        finalHomeScore: number,
+        firstHalfAwayScore: number,
+        firstHalfHomeScore: number,
+        secondHalfAwayScore: number,
+        secondHalfHomeScore: number,
+        overtimeAwayScore: number,
+        overtimeHomeScore: number,
+        penaltyAwayScore: number,
+        penaltyHomeScore: number
+    }
 }
 
 
 const MainSection = ({MatchId, SportType} : { MatchId : number; SportType : string;}) => {
 
 
-    const [match, setMatch] = useState<BasketballMatchData | HockeyMatchData | FootballMatchData | null>(null);
+    const [match, setMatch] = useState<BasketballMatchData | HockeyMatchData | FootballMatchData | null>(
+        {
+            id: 1,
+            date: "2021-05-05",
+            time: "20:00",
+            homeTeamId: 1,
+            awayTeamId: 2,
+            homeTeamName: "FC Barcelona",
+            awayTeamName: "Real Madrid",
+            homeTeamLogo: sparta,
+            awayTeamLogo: slavia,
+            alreadyPlayed: false,
+            isFavourite: false,
+            score : {
+                finalAwayScore: 1,
+                finalHomeScore: 2,
+                firstQuarterAwayScore: 1,
+                firstQuarterHomeScore: 1,
+                secondQuarterAwayScore: 0,
+                secondQuarterHomeScore: 1,
+                thirdQuarterAwayScore: 0,
+                thirdQuarterHomeScore: 0,
+                fourthQuarterAwayScore: 0,
+                fourthQuarterHomeScore: 0,
+                overtimeAwayScore: 0,
+                overtimeHomeScore: 0
+            }
+        }
+    );
 
     const fetchMatches = async () => {
         try {
@@ -107,6 +141,28 @@ const MainSection = ({MatchId, SportType} : { MatchId : number; SportType : stri
     }, []);
 
 
+    let isVisibleHomeTeamLogo = false;
+    let isVisibleAwayTeamLogo = false;
+    let isVisibleHomeTeamScore = false;
+    let isVisibleAwayTeamScore = false;
+    let isFutureMatch = false;
+
+    const isVisibleMap = () => {
+        if (match.homeTeamLogo !== null) {
+            isVisibleHomeTeamLogo = true;
+        }
+        if (match.awayTeamLogo !== null) {
+            isVisibleAwayTeamLogo = true;
+        }
+        if (match.score.finalHomeScore !== null) {
+            isVisibleHomeTeamScore = true;
+        }
+        if (match.score.finalAwayScore !== null) {
+            isVisibleAwayTeamScore = true;
+        }
+    }
+
+    isVisibleMap();
 
 
     const teamOnClicked = (teamName : string) : MouseEventHandler<HTMLButtonElement> => {
@@ -117,28 +173,51 @@ const MainSection = ({MatchId, SportType} : { MatchId : number; SportType : stri
         <section className="MainSection">
             <div className={`MainSectionContainer flex justify-center`}>
                 <div className="imgFotbalista"></div>
-
-
-
-
-                <div className={`placeholder p-4 m-10 grid gap-4 grid-rows-2`}>
-                    <div className={`inline-grid grid-cols-3 gap-4`}>
-                        <div>
-                            <img className={`content-center object-contain h-15 w-15`} src={"____"} alt="Team" />
-                            <button onClick={teamOnClicked(match.homeTeamName)} className={`pl-3 content-center ${styles.team}`}>{match.homeTeamName}</button>
+                <div className={`placeholder p-4 mx-10 grid gap-4 grid-rows-5 auto-rows-min`}>
+                    <div className={`row-span-2 flex justify-around auto-cols-min detailHeader {/*px-20*/}`}>
+                        <div className={`flex flex-col place-content-evenly`}>
+                            <img className={`object-contain h-32`} src={match.homeTeamLogo} alt="Team" />
+                            <button onClick={teamOnClicked(match.homeTeamName)} className={`teamButton`}>{match.homeTeamName}</button>
                         </div>
-                        <div>
-
+                        <div className={`flex flex-col content-center justify-center`}>
+                            <div className={`scorePlaceholder flex  justify-center px-12 pt-0.5`}>
+                                {isFutureMatch ? (
+                                            <p className={`scorePlaceholderText pt-0.5 pb-0.2`}>Nadcházející</p>
+                                ) : (
+                                    <>
+                                        {isVisibleHomeTeamScore ? (
+                                            <p className={`scorePlaceholderText pr-4 pt-0.5 pb-0.2`}>{match.score.finalHomeScore}</p>
+                                        ) : (
+                                            <div />
+                                        )}
+                                            <p className={`scorePlaceholderText  pt-0.5 pb-0.3`}>:</p>
+                                        {isVisibleAwayTeamScore ? (
+                                            <p className={`scorePlaceholderText pl-4 pt-0.5 pb-0.2`}>{match.score.finalAwayScore}</p>
+                                        ) : (
+                                            <div />
+                                        )}
+                                    </>
+                                )}
+                            </div>
                         </div>
-                        <div>
-
+                        <div className={`flex flex-col place-content-evenly`}>
+                            <img className={`object-contain h-32`} src={match.awayTeamLogo} alt="Team" />
+                            <button onClick={teamOnClicked(match.homeTeamName)} className={`teamButton`}>{match.homeTeamName}</button>
                         </div>
                     </div>
-                    <div className={``}>
+                    <div className={`row-span-3 grid grid-flow-row auto-rows-max`}>
+
+                        <MatchDetailedScore text={"Poločas"} homeScore={1} awayScore={0}/>
+
+
+
+
 
                     </div>
 
                 </div>
+
+
 
 
 
---
+++ frontend/src/components/MatchDetailedScore/MatchDetailedScore.tsx
@@ -0,0 +1,58 @@
+import styles from './MatchDetailedScore.module.css';
+import sparta from '../../assets/sparta.png';
+import slavia from '../../assets/slavia.png';
+import ZobrazitVice from "../Buttons/ZobrazitVice/ZobrazitVice";
+import {isVisible} from "@testing-library/user-event/dist/utils";
+import {useState} from "react";
+import {useLocation} from 'react-router-dom';
+
+function MatchDetailedScore({text, homeScore, awayScore} : {
+    text: string;
+    homeScore: number | null;
+    awayScore: number | null;
+}) {
+
+    const location = useLocation();
+
+    let isVisibleHomeScore = false;
+    let isVisibleAwayScore = false;
+
+    const isVisibleMap = () => {
+        if (homeScore !== null) {
+            isVisibleHomeScore = true;
+        }
+        if (awayScore !== null) {
+            isVisibleAwayScore = true;
+        }
+    }
+
+    isVisibleMap();
+
+    return (
+        <div className={`inline-grid gap-3 ${styles.match}`}>
+            <div className="columns-1 shrink">
+                <div className={`flex justify-center align-items-middle shrink mt-2 mb-1.5 p-1 `}>
+                    <p className={`${styles.textBorder} pb-1 pt-1.5 px-3.5`}>{text}</p>
+                </div>
+                <div className={`flex justify-center mb-3`}>
+                    <div className={`${styles.score} flex justify-center px-4 pt-0.5`}>
+                        {isVisibleHomeScore ? (
+                            <p className={`${styles.score1} pr-4 pt-0.4 pb-0.2`}>{homeScore}</p>
+                        ) : (
+                            <div />
+                        )}
+                        <p className={`${styles.colon} pt-0.3 pb-0.3`}>:</p>
+                        {isVisibleAwayScore ? (
+                            <p className={`${styles.score2} pl-4 pt-0.4 pb-0.2`}>{awayScore}</p>
+                        ) : (
+                            <div />
+                        )}
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+
+}
+export default MatchDetailedScore;
