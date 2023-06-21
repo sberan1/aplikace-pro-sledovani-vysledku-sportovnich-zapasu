ffcb59452180a7f12b906ab094f010df4245e5f8
Julie Sanetrníková <sanj10@vse.cz>
2023-06-21 10:45:20 +0200

    dodelam az po zamergovani

--- frontend/src/components/HeaderTymu/HeaderTymu.tsx
+++ frontend/src/components/HeaderTymu/HeaderTymu.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './HeaderTymu.css';
-import FavoriteTeamBtn from "../FavoriteTeamBtn/FavoriteTeamBtn";
+import FavoriteTeamBtn from "../buttons/FavoriteTeamBtn/FavoriteTeamBtn";
 
 async function getTeamData(teamId) {
 
